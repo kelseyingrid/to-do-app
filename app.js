3897acedb2558362815aa526276078d943d09d76
Kelsey Jacobsen <kelseyingrid@gmail.com>
2019-05-07 13:50:55 -0700

    assignment 5: events part 1 completed

--- app.js
+++ app.js
@@ -15,23 +15,45 @@ function onReady() {
     // create new input
     let checkbox = document.createElement('input');
 
+    // create delete button
+    let deleteButton = document.createElement('button');
+
     // set the input's type to checkbox
     checkbox.type = "checkbox";
+
+    // set the content of the deleteButton
+    deleteButton.textContent = "Delete";
+
+    // set the button type to submit
+    deleteButton.type = "submit";
 
     // set the title
     newLi.textContent = title;
 
-    // attach the checkbok to the li
+    // attach the checkbox to the li
     newLi.appendChild(checkbox);
 
     // attach the li to the ul
     toDoList.appendChild(newLi);
 
+    // attach the delete button to the li
+    newLi.appendChild(deleteButton);
+
     // empty the input
     newToDoText.value = ''
 
+    // add event listener for delete button click
+    deleteButton.addEventListener('click', function(event) {
+
+      // remove parent li
+      toDoList.removeChild(this.parentElement);
+
+  })
+
   });
+
 }
+
 
 window.onload = function() {
   onReady();
